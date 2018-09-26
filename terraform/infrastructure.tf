874fc805bf6377a04a4e5fe865305921891b31a4
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2018-09-26 18:01:51 +0300

    fix wrong parameter

--- terraform/infrastructure.tf
+++ terraform/infrastructure.tf
@@ -145,7 +145,7 @@ resource "aws_instance" "app1" {
   instance_type = "t2.micro"
   subnet_id = "${aws_subnet.subnet1.id}"
   key_name = "${aws_key_pair.keypair.key_name}"
-  security_groups = ["${aws_security_group.ssh.id}"]
+  vpc_security_group_ids = ["${aws_security_group.ssh.id}"]
   tags = "${merge(
     local.common_tags,
     map(
